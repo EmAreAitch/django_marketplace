f3686816ef2f5fbdee6f95cae523b4adacb16b09
emareaitch <dev.emareaitch@gmail.com>
2024-05-07 16:44:05 +0530

    Final url mapping

--- django_marketplace/urls.py
+++ django_marketplace/urls.py
@@ -23,6 +23,8 @@ from core import views
 urlpatterns = [
     path('',include('core.urls')),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls')),
     path('contact/',views.contact,name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
